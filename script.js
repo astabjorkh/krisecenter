0e5ba1d9a1ce18a460cf8789de3c68fa0a985d40
Asta Hardar <91729063+astabjorkh@users.noreply.github.com>
2022-06-01 17:38:54 +0200

    images and more

--- script.js
+++ script.js
@@ -2,20 +2,13 @@ console.log("script loaded");
 const voldLinks = document.querySelectorAll(".vold__link");
 const voldSlides = document.querySelectorAll(".vold__slide");
 
-// create click events for the links
-voldLinks.forEach(link => {
-    console.log(link);
-    link.addEventListener("click",(event)=>{
-        event.preventDefault();
-        showHideArticles(event.target.dataset.slide);
-        event.target.classList.add("active");
-        // TODO: remember to loop here and remove all previous "active" classes!
-    });
-});
-
-
-
-
+function resetAllLinksActive() {
+  // removes the class "active" from all links, here it performes the action
+  voldLinks.forEach(link =>{
+    link.classList.remove('active');
+  });
+}
+// when you click on one then the active status is removed from others
 function showHideArticles(id){
     console.log("clicked",id);
     voldSlides.forEach(text => {
@@ -29,13 +22,29 @@ function showHideArticles(id){
 }
 
 
+// create click events for the links
+voldLinks.forEach(link => {
+    console.log(link);
+    link.addEventListener("click",(event)=>{
+        event.preventDefault();
+        showHideArticles(event.target.dataset.slide);
+        resetAllLinksActive();
+        event.target.classList.add("active");
+        // it removes all active classes!
+    });
+});
 
 
+
+
+
+
+// source : https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_to_top */
 
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When you scroll down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -46,8 +55,10 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// When you click on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
